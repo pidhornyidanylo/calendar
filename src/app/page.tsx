cc69077d89afd56f96ee3baebf5e0b5414a99f60
dania <dquark74@protonmail.com>
2024-08-08 11:46:29 +0300

    added getStaticProps to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,23 @@ import HomeHeader from "@/components/HomeComponents/homeHeader/HomeHeader";
 import HomeSchedule from "@/components/HomeComponents/homeSchedule/HomeSchedule";
 import { getTasks } from "@/lib/data";
 
-const Home = async () => {
-  const schedule = await getTasks();
+const Home = ({ schedule }: { schedule: string }) => {
   return (
     <>
       <HomeHeader />
-      <HomeSchedule data={JSON.stringify(schedule)} />
+      <HomeSchedule data={schedule} />
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const schedule = await getTasks();
+  return {
+    props: {
+      schedule: JSON.stringify(schedule),
+    },
+    revalidate: 60,
+  };
 };
 
 export default Home;
