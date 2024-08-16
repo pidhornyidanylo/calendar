e7ca2e91f6629fd4982ddf08ead9c608b49ecc2c
dania <dquark74@protonmail.com>
2024-08-16 15:36:24 +0300

    upd data.ts

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,19 +1,24 @@
 import HomeHeader from "@/components/HomeComponents/homeHeader/HomeHeader";
 import HomeSchedule from "@/components/HomeComponents/homeSchedule/HomeSchedule";
+import Spinner from "@/components/reusable/Spinner/Spinner";
 import { getTasks } from "@/lib/data";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import type React from "react";
 
 const Home: React.FC = async () => {
-	const { getUser } = getKindeServerSession();
-	const user = await getUser();
-	const schedule = await getTasks(user?.id as string);
-	return (
-		<>
-			<HomeHeader />
-			<HomeSchedule data={JSON.stringify(schedule)} />
-		</>
-	);
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (user) {
+    const schedule = await getTasks(user?.id as string);
+    return (
+      <>
+        <HomeHeader />
+        <HomeSchedule data={JSON.stringify(schedule.tasks)} />
+      </>
+    );
+  } else {
+    return <Spinner />;
+  }
 };
 
 export const revalidate = 60;
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,30 +3,33 @@ import { connectToDb } from "./db";
 import { UserModel } from "./models";
 
 export const getTasks = async (token: string) => {
-	try {
-		await connectToDb();
-		const dbUSer = await UserModel.findOne({ token: token });
-		return dbUSer.tasks;
-	} catch (error) {
-		throw new Error("Error fetching tasks!");
-	}
+  try {
+    await connectToDb();
+    const dbUSer = await UserModel.findOne({ token: token });
+    if (dbUSer) {
+      return { success: true, tasks: dbUSer.tasks };
+    }
+    return { success: false, tasks: [] };
+  } catch (error) {
+    throw new Error("Error fetching tasks!");
+  }
 };
 
 export const getTheme = async (token: string) => {
-	try {
-		await connectToDb();
-		const dbUSer = await UserModel.findOne({ token: token });
-		if (dbUSer) {
-			return { success: true, theme: dbUSer.theme };
-		}
-		const newDbUser = new UserModel({
-			token: token,
-			theme: "light",
-			tasks: [],
-		});
-		await newDbUser.save();
-		return { success: true, theme: "light" };
-	} catch (error) {
-		return { success: false, message: "Error fetching theme!" };
-	}
+  try {
+    await connectToDb();
+    const dbUSer = await UserModel.findOne({ token: token });
+    if (dbUSer) {
+      return { success: true, theme: dbUSer.theme };
+    }
+    const newDbUser = new UserModel({
+      token: token,
+      theme: "light",
+      tasks: [],
+    });
+    await newDbUser.save();
+    return { success: true, theme: "light" };
+  } catch (error) {
+    return { success: false, message: "Error fetching theme!" };
+  }
 };
